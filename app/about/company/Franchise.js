93f74e7a34e0f5aa79dd946c192e1ef094947c66
Beth Wickerson <beth.wickerson@signaltheory.com>
2024-01-08 11:15:21 -0600

    company franchise btn

--- app/about/company/Franchise.js
+++ app/about/company/Franchise.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Franchise = () => {
   return (
@@ -17,6 +18,7 @@ const Franchise = () => {
               <h2>FRANCHISING OPPORTUNITIES AWAIT</h2>
               <p>Our vision is to be the best gourmet pizza in every market we serve. Our excellence has grown from practices started in Italy to our dozens of successful franchisees across the world. The simple operation, limited cooking, and traditional Italian recipes are what make Sarpino&apos;s work for our franchised system.</p>
               <p>We have created several options for investment to fit mostly any location, or locate, that allows our franchisees to get up and running as quickly as possible with an efficient capital structure and efficiency of operations. We have over 80 restaurants are open in 12 countries with 50 under development. We have opportunities in many areas, so take the next step and apply to be a Sarpino&apos;s franchise partner today.</p>
+              <Link className='btn primary-btn' style={{margin: '2rem auto 1rem 0'}} href="/franchise">Learn more about our franchise</Link>
             </div>
         </div>
       </div>
