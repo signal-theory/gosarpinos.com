21f67a82ed4fc8b42280383e0f913b2623c23534
Beth Wickerson <beth.wickerson@signaltheory.com>
2024-01-19 15:55:12 -0600

    loyalty animation

--- app/loyalty-program/Hero.js
+++ app/loyalty-program/Hero.js
@@ -26,6 +26,9 @@ const Hero = ({ data, mainImage }) => {
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw"
           />
         )}
+        <Image src={'/heart-red-fill.svg'} width={55} height={55} className={styles.redFillHeart} alt="red fill heart" />
+        <Image src={'/heart-green-outline.svg'} width={57} height={57} className={styles.greenOutlineHeart} alt="green outline heart" />
+        <Image src={'/heart-red-outline.svg'} width={60} height={60} className={styles.redOutlineHeart} alt="red outline heart" />
       </div>
     </div>
   );
