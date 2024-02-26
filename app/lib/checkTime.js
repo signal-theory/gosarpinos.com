c8e804237ea8a1cc159448ebde733842607c89f4
Beth Wickerson <beth.wickerson@signaltheory.com>
2024-02-26 12:55:42 -0600

    force isDay true

--- app/lib/checkTime.js
+++ app/lib/checkTime.js
@@ -2,9 +2,10 @@ import moment from 'moment-timezone';
 
 export function checkTime() {
   // Get the current hour in a specific timezone
-  const currentHour = moment().hour();
-  // Check if the current hour is between 6 and 16
-  const isDay = currentHour >= 6 && currentHour < 16;
-  console.log('isDay:', isDay, ' currentHour:', currentHour);
+  // const currentHour = moment().hour();
+  // // Check if the current hour is between 6 and 16
+  // const isDay = currentHour >= 6 && currentHour < 16;
+  // console.log('isDay:', isDay, ' currentHour:', currentHour);
+  const isDay = true;
   return isDay;
 }
