5ba960b8761c6f061848b7e2cf6beac14b4759b5
Beth Wickerson <beth.wickerson@signaltheory.com>
2024-02-26 12:49:29 -0600

    client side checkTime with moment-timezone

--- app/lib/checkTime.js
+++ app/lib/checkTime.js
@@ -1,7 +1,9 @@
+import moment from 'moment-timezone';
+
 export function checkTime() {
-  // Get the current hour
-  const currentHour = new Date().getHours();
-  // Check if the current hour is between 7 and 16
+  // Get the current hour in a specific timezone
+  const currentHour = moment().hour();
+  // Check if the current hour is between 6 and 16
   const isDay = currentHour >= 6 && currentHour < 16;
   console.log('isDay:', isDay, ' currentHour:', currentHour);
   return isDay;
