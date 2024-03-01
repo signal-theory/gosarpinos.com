0dcef59a2776a31de54b4798dcd6b82975c4361e
Beth Wickerson <beth.wickerson@signaltheory.com>
2024-03-01 12:44:49 -0600

    default day theme

--- app/context/useThemeProvider.js
+++ app/context/useThemeProvider.js
@@ -1,12 +1,16 @@
 'use client'
-import { createContext } from 'react'
+import { createContext, useEffect, useState } from 'react'
 import { checkTime } from '../lib/checkTime';
 
 export const ThemeContext = createContext({})
 
 export const ThemeProvider = ({ children }) => {
-  const isDay = checkTime();
-  const theme = isDay ? 'day' : 'night';
+  const [theme, setTheme] = useState('day');
+
+  useEffect(() => {
+    const isDay = checkTime();
+    setTheme(isDay ? 'day' : 'night');
+  }, []);
 
   return <ThemeContext.Provider value={theme}>
     <main className={theme}>
--- app/lib/checkTime.js
+++ app/lib/checkTime.js
@@ -5,9 +5,13 @@ export function checkTime() {
   const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
   const currentHour = moment().tz(timeZone).hour();
   console.log('timeZone', timeZone);
-  console.log('moment', moment().tz(timeZone).format());
+
+  const serverTimezone = moment.tz.guess();
+  console.log('Server timezone:', serverTimezone);
+
   const isDay = currentHour >= 6 && currentHour < 16;
   console.log('currentHour', currentHour);
   console.log('isDay', isDay);
+
   return isDay;
 }
