8b09f156894d6fe28b18aa3b2e18f9a0d73410ca
Beth Wickerson <beth.wickerson@signaltheory.com>
2024-03-05 15:53:45 -0600

    franchise cms, remove vegan desserts

--- app/about/company/page.js
+++ app/about/company/page.js
@@ -1,5 +1,5 @@
 import { METADATABASE_API_URL } from '../../lib/constants';
-import { fetchMetadata, fetchPageData } from '../../lib/utils'; // Adjust the path as necessary
+import { fetchMetadata, fetchPageData, fetchACFImage } from '../../lib/utils'; // Adjust the path as necessary
 import Hero from './Hero';
 import Ingredients from './Ingredients';
 import Timeline from './Timeline';
@@ -26,9 +26,11 @@ export async function generateMetadata() {
 
 export default async function Page({ params }) {
   let data;
+  let franchiseImage;
 
   try {
     data = await fetchPageData(pageId);
+    franchiseImage = data.acf && data.acf.franchise_image ? await fetchACFImage(data.acf.franchise_image) : null;
 
   } catch (error) {
     console.error("Error in Page component:", error);
@@ -41,7 +43,7 @@ export default async function Page({ params }) {
         <Hero data={data} />
         <Ingredients data={data} />
         <Timeline data={data} />
-        <Franchise />
+        <Franchise data={data} franchiseImage={franchiseImage} />
       </div>
     </>
   );
