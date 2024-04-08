710395ae2fd4dea0571715a6ab7aa02ee65f8191
Beth Wickerson <beth.wickerson@signaltheory.com>
2024-04-08 08:55:21 -0500

    env variable REVIEW_TOKEN

--- app/pizza-delivery/[slug]/Feedback.js
+++ app/pizza-delivery/[slug]/Feedback.js
@@ -2,7 +2,7 @@ import Script from 'next/script';
 import styles from './Feedback.module.css';
 const Feedback = ({ post }) => {
   return (
-    <div className={styles.container}><div className="amplify-widget" data-token="MTEyMjY6MmlSWW5jTlAxQk1EOVJocmwyVFpjV2NVWHp3" data-widget-id="11226" data-widget-type="full_page" data-external-id={post.acf.review_tracker_id}></div>
+    <div className={styles.container}><div className="amplify-widget" data-token={globalThis.NEXT_PUBLIC_REVIEW_TOKEN ?? (process.env.NEXT_PUBLIC_REVIEW_TOKEN)} data-widget-id="11226" data-widget-type="full_page" data-external-id={post.acf.review_tracker_id}></div>
       <Script src="https://amplify.review-alerts.com/widget-init.js" />
     </div>
   );
