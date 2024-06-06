2e69ee7538d4f24eb4151c49d716a6ec8d627579
David Harris <dsharris@Davids-MacBook-Pro.local>
2024-06-06 00:31:04 -0500

    Added the import of redirects into redis with code commented out, added the middleware

---
+++ middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server'
+import { createClient } from '@vercel/kv';
+
+export async function middleware(request) {
+    const kv = createClient({
+        url: process.env.KV_REST_API_URL,
+        token: process.env.KV_REST_API_TOKEN,
+    });
+
+    const pathname = request.nextUrl.pathname
+    const redirectData = await kv.get(pathname);
+    
+    if (redirectData) {
+        return NextResponse.redirect(request.nextUrl.origin+redirectData, 308)
+    }
+ 
+    return NextResponse.next()
+}
